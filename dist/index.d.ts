994f3f1c4b1b9cb034d1649a62fe5af996ae3e8b
raagan-u <n0r313n7@protonmail.ch>
2024-10-09 15:39:33 +0530

    husky setup info

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export type ExpectedType = 'string' | 'number' | 'boolean' | 'object';
-export interface ValidationSchema {
-    [key: string]: ExpectedType | ValidationSchema;
-}
-export declare function typeChecker(request: any, schema: ValidationSchema): boolean;
